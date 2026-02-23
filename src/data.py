c61d69b2a8772fa71a53482750eb012bdf32d8a5
karthikeyan0305 <karthikeyan.03r@gmail.com>
2026-02-23 17:27:35 +0530

    coomit

--- src/data.py
+++ src/data.py
@@ -1,9 +1,8 @@
+
+
 import pandas as pd
 
 def load_data(path):
     df = pd.read_csv(path)
-
-    # fill missing numeric values
     df.fillna(df.mean(numeric_only=True), inplace=True)
-
     return df
